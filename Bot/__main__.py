49afa177fc5b69e3db1a818725494c9747fb9d67
Chizuru <chizuru@kanojo.tk>
2020-10-16 23:41:54 +0530

    add .ping
    oh well

--- Bot/__main__.py
+++ Bot/__main__.py
@@ -6,7 +6,7 @@ import traceback
 import logging
 from pyrogram import  idle, Client
 from Bot import app
-from Bot.modules import alive, admin, pin, whois, dev, help
+from Bot.modules import alive, admin, pin, whois, dev, help, ping
 
 
 app.start()
---
+++ Bot/modules/ping.py
@@ -0,0 +1,12 @@
+from datetime import datetime
+from Bot import app
+from pyrogram.types import Message
+from pyrogram import filters
+
+@app.on_message(filters.command('ping', '.') & filters.me)
+async def pingme(_, message: Message):
+    start = datetime.now()
+    await message.edit('`Pong!`')
+    end = datetime.now()
+    m_s = (end - start).microseconds / 1000
+    await message.edit(f"**Pong!**\n`{m_s} ms`")
